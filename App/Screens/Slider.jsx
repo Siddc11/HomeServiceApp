d38808025f79de72b25e49f6780509708f606537
Siddhesh Chaudhari <siddheshchaudhari910@gmail.com>
2024-05-08 16:55:35 +0530

    Fetching Sliders from backend done

---
+++ App/Screens/Slider.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../Utils/GlobalApi'
+
+export default function Slider() {
+    
+    const [slider, setSlider] = useState();
+    useEffect(()=>{
+     getSlider();
+    },[])
+
+    const getSlider =()=>{
+        GlobalApi.getSlider().then(resp=>{
+            console.log("resp", resp.sliders);
+            setSlider(resp?.sliders)
+        })
+    }
+  return (
+    <View>
+      <Text>Slider</Text>
+    </View>
+  )
+}
---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,28 @@
+import { request, gql } from 'graphql-request';
+
+const URL = "https://api-ap-south-1.hygraph.com/v2/clvxktxdt08su07w5x4c5k20l/master";
+
+const getSlider = async () => {
+    const query = gql`
+        query GetSlider {
+            sliders {
+                id
+                image {
+                    url
+                }
+            }
+        }
+    `;
+
+    try {
+        const result = await request(URL, query);
+        return result;
+    } catch (error) {
+        console.error("Error fetching slider data:", error);
+        return null;
+    }
+};
+
+export default {
+    getSlider
+};
