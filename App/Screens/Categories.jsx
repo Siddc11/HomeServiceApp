4835236819b1a7eaf4e2c02d6453ae1a7f627ae8
Siddhesh Chaudhari <siddheshchaudhari910@gmail.com>
2024-05-11 23:58:56 +0530

    minor changes

--- App/Screens/Categories.jsx
+++ App/Screens/Categories.jsx
@@ -1,36 +1,37 @@
-import { View, Text, FlatList, Image } from "react-native";
-import React, { useEffect, useState } from "react";
-import GlobalApi from "../Utils/GlobalApi";
-import Heading from "../Components/Heading";
+import { View, Text, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../Utils/GlobalApi';
+import Heading from '../Components/Heading';
 
-const Categories = () => {
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    getCategories();
-  }, []);
-  const getCategories = () => {
-    GlobalApi.getCategory().then((resp) => {
-      setCategories(resp?.categories);
-    });
-  };
+const Categories=()=> {
+
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        getCategories();
+    },[])
+    const getCategories=()=>{
+        GlobalApi.getCategory().then(resp=>{
+            setCategories(resp?.categories)
+        })
+    }
   return (
-    <View style={{ marginTop: 10 }}>
-      <Heading text={"Categories"} isViewAll={true} />
+    <View style={{marginTop: 10}}>
+     <Heading text={'Categories'} isViewAll={true}/>
 
-      <FlatList
-        data={categories}
-        renderItem={({ item, index }) => (
-          <View>
+     <FlatList
+     data={categories}
+     renderItem={({item, index})=>{
+        <View>
             <View>
-              <Image
-                source={{ uri: item?.icon?.url }}
-                style={{ width: 30, height: 30 }}
-              />
+                <Image 
+                  source={{uri:item?.icons?.url}}
+                  style={{width:30, height:30}}
+                />
             </View>
-          </View>
-        )}
-      />
+        </View>
+     }}
+     />
     </View>
-  );
-};
+  )
+}
 export default Categories;
